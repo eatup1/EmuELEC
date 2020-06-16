1b19c3c025b31bbba5760955e3d3c1d85e878fac
Youngjun, Choi <british2@naver.com>
2020-06-16 21:24:48 +0900

    emuelec-emulationstation: revert to e7a114d

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="c766cbdb091dc5c6cd59eefb1278e44084b08808"
+PKG_VERSION="e7a114d9605b684313f6472de9e8e5bba5f42ef9"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
