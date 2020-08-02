2e4a9639cee2ff8d70f6c00b16fe3a3b88aac7dc
Youngjun, Choi <british2@naver.com>
2020-08-02 18:06:52 +0900

    ES: bump to 14e1526

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="b986a5874a9c99bce7f5284c997ee838de979aa4"
+PKG_VERSION="14e1526fc583a414868f5493991c6320f6ce5d62"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
