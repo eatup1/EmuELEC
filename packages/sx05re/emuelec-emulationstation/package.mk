82caa33c1f7dfb6cbaf2d980e9cc30a091e48419
Youngjun, Choi <british2@naver.com>
2020-10-25 14:51:16 +0900

    Emuelec-emulationstation: Bump to 6931fcc

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="23201043fb4d79f4714580b174fba1a5f5693735"
+PKG_VERSION="6931fcc85d9193c5c3bd5e052068c2ca88c42fea"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
