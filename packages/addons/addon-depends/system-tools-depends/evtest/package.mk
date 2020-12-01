3f867911950c98f63cf92ee84ca5261c64e25935
Youngjun, Choi <british2@naver.com>
2020-12-01 10:15:21 +0900

    evtest : bump to 1.34

--- packages/addons/addon-depends/system-tools-depends/evtest/package.mk
+++ packages/addons/addon-depends/system-tools-depends/evtest/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="evtest"
-PKG_VERSION="1.33"
-PKG_SHA256="5037d1162f4c407053cd97e85763ba03150a0c35f929ee9bf9a360abd32ef1c1"
+PKG_VERSION="1.34"
+PKG_SHA256="4c24427792393d6b68fe4010513c6dbb606dd6eb3cea5c2b8c9eb705551cb50a"
 PKG_LICENSE="GPL"
 PKG_SITE="http://cgit.freedesktop.org/evtest/"
-PKG_URL="http://cgit.freedesktop.org/evtest/snapshot/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="https://gitlab.freedesktop.org/libevdev/evtest/-/archive/master/$PKG_NAME-$PKG_VERSION.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain libxml2"
 PKG_LONGDESC="A simple tool for input event debugging."
 PKG_TOOLCHAIN="autotools"
