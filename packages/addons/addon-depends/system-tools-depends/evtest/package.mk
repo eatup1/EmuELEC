1a76d0c1589c5ffd3e7f193702e524cb25b4a08d
Youngjun, Choi <british2@naver.com>
2021-03-05 22:55:19 +0900

    evtest : Fix compile error

--- packages/addons/addon-depends/system-tools-depends/evtest/package.mk
+++ packages/addons/addon-depends/system-tools-depends/evtest/package.mk
@@ -3,7 +3,7 @@
 
 PKG_NAME="evtest"
 PKG_VERSION="1.34"
-PKG_SHA256="abc8439f41a73b09543fe2474a78a819a386d92d9aabf33b67ccbad8a8b09fd4"
+PKG_SHA256="f8a2a790e0116979e6fd64d933e20ab067bc48aa70f2416834e05cfe11ed5a32"
 PKG_LICENSE="GPL"
 PKG_SITE="http://cgit.freedesktop.org/evtest/"
 PKG_URL="https://gitlab.freedesktop.org/libevdev/evtest/-/archive/master/$PKG_NAME-$PKG_VERSION.tar.bz2"
