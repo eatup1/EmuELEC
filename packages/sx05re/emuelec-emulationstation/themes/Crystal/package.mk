01714023f101d7ac5133589a33e59771f6905cec
Youngjun, Choi <british2@naver.com>
2021-03-14 12:21:19 +0900

    Update Crystal theme

--- packages/sx05re/emuelec-emulationstation/themes/Crystal/package.mk
+++ packages/sx05re/emuelec-emulationstation/themes/Crystal/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="Crystal"
-PKG_VERSION="0ae5fe3192db790c9e054349c5e1594a26860ae8"
+PKG_VERSION="592ca1fa4f20d778099e102bc7f1e8c9cd76c661"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/dm2912/Crystal"
+PKG_SITE="https://github.com/british-choi/Crystal"
 PKG_URL="$PKG_SITE.git"
 PKG_DEPENDS_TARGET="toolchain Crystal-Collections"
 PKG_SECTION="emuelec"
@@ -22,28 +22,4 @@ makeinstall_target() {
   mkdir -p $INSTALL/usr/config/emulationstation/themes/Crystal
     cp -r * $INSTALL/usr/config/emulationstation/themes/Crystal
     rm -rf $INSTALL/usr/config/emulationstation/themes/Crystal/screens.png
-
-    # add korean lang
-    cp -r $PKG_DIR/lang/* $INSTALL/usr/config/emulationstation/themes/Crystal/lang
-
-    # Change to korean font
-    sed -i "s|Homizio.ttf|NanumGothic.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/gamelists.xml
-    sed -i "s|OpenSans-Bold.ttf|NanumGothicExtraBold.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/systemview/common.xml
-    sed -i "s|OpenSans-Bold.ttf|NanumGothicExtraBold.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/systemview/details.xml
-    sed -i "s|OpenSans-Bold.ttf|NanumGothicExtraBold.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/systemview/horizontal_details.xml
-    sed -i "s|OpenSans-Homizio.ttf|NanumGothic.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/gamelists.xml
-    sed -i "s|OpenSans-Homizio.ttf|NanumGothic.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/theme.xml
-    sed -i "s|OpenSans-Light.ttf|NanumGothic.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/theme.xml
-    sed -i "s|OpenSans-Semibold.ttf|NanumGothicBold.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/gamelists.xml
-    sed -i "s|OpenSans-Semibold.ttf|NanumGothicBold.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/theme.xml
-    sed -i "s|OpenSans-Semi-Bold.ttf|NanumGothicBold.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/systemview/details.xml
-    sed -i "s|OpenSans-Semi-Bold.ttf|NanumGothicBold.ttf|" $INSTALL/usr/config/emulationstation/themes/Crystal/systemview/horizontal_details.xml
-
-    # Copy korean nanum font & Remove unused font
-    cp -r $PKG_DIR/font/* $INSTALL/usr/config/emulationstation/themes/Crystal/fonts
-    rm -rf $INSTALL/usr/config/emulationstation/themes/Crystal/fonts/Homizio.ttf
-    rm -rf $INSTALL/usr/config/emulationstation/themes/Crystal/fonts/OpenSans-Bold.ttf
-    rm -rf $INSTALL/usr/config/emulationstation/themes/Crystal/fonts/OpenSans-Homizio.ttf
-    rm -rf $INSTALL/usr/config/emulationstation/themes/Crystal/fonts/OpenSans-Light.ttf
-    rm -rf $INSTALL/usr/config/emulationstation/themes/Crystal/fonts/OpenSans-Semibold.ttf
 }
