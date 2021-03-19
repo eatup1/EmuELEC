3646bc7d9bd9b8c2aec9142d3594c36fcb527f05
Youngjun, Choi <british2@naver.com>
2021-03-19 21:34:40 +0900

    youtube-dl: bump to 2021.03.14

--- packages/sx05re/tools/video/youtube-dl/package.mk
+++ packages/sx05re/tools/video/youtube-dl/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="youtube-dl"
-PKG_VERSION="2021.01.24.1"
-PKG_SHA256="cb9f350dde08685c8f841818b6fe54271361f590570554313a897810f0240a8a"
+PKG_VERSION="2021.03.14"
+PKG_SHA256="3595d61a08966b940681842f002cff93e9199dbe1f280775d956762f8fe6b927"
 PKG_LICENSE="The Unlicense"
 PKG_SITE="https://github.com/ytdl-org/youtube-dl"
 PKG_URL="${PKG_SITE}/releases/download/${PKG_VERSION}/youtube-dl"
