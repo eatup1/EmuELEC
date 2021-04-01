f8be6293db5f51586f593616259ddca3b5a65661
Youngjun, Choi <british2@naver.com>
2021-04-02 00:24:58 +0900

    Retroarch: fix compile error

--- packages/sx05re/libretro/retroarch/package.mk
+++ packages/sx05re/libretro/retroarch/package.mk
@@ -19,11 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-<<<<<<< HEAD
-PKG_VERSION="01944dd80ea79fefb335f8f2c437fc1c106482cf"
-=======
-PKG_VERSION="cf6ca2a47f44126f4caf9242cfa7917b94abbc52"
->>>>>>> 1af7d7b461f527bec7dd2fcaef83ad2cedbacd5e
+PKG_VERSION="21b2ec6bd528a6acc9fc145af9b057ad27213135"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="$PKG_SITE.git"
 PKG_LICENSE="GPLv3"
