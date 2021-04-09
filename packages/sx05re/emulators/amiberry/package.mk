02dffc0b51e0a1746677bf227225d9e7be0a6e83
Youngjun, Choi <british2@naver.com>
2021-04-09 15:55:37 +0900

    amiberry: Bump to b86b03f (v4.1.2)

--- packages/sx05re/emulators/amiberry/package.mk
+++ packages/sx05re/emulators/amiberry/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present Frank Hartung (supervisedthinking (@) gmail.com)
 
 PKG_NAME="amiberry"
-PKG_VERSION="d4176b4a2627fb0e9cbcb26f7c6665d95e1d3303"
+PKG_VERSION="b86b03ffc4578a7a24c6e3bfe7bf661dba86c6b9"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/midwan/amiberry"
 PKG_URL="https://github.com/midwan/amiberry.git"
