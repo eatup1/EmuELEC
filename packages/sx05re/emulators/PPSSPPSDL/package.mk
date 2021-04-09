85cc56fd0951ed0c5bc676d5b80f9e2db6e4b806
Youngjun, Choi <british2@naver.com>
2021-04-09 15:51:49 +0900

    ppsspp: Bump to 6f332dd

--- packages/sx05re/emulators/PPSSPPSDL/package.mk
+++ packages/sx05re/emulators/PPSSPPSDL/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="PPSSPPSDL"
-PKG_VERSION="3a83b96f1a9b45a83bf6acbe2e4b72f2fd5165f1"
+PKG_VERSION="6f332dd66401846e3e4f97262ac4d0d31cc54d97"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
