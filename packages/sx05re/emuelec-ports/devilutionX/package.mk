5909bdce69c9857ec55bfe309e3255a7da676f89
Youngjun, Choi <british2@naver.com>
2021-07-10 00:22:35 +0900

    devilutionX: Add reverse ABXY patch

--- packages/sx05re/emuelec-ports/devilutionX/package.mk
+++ packages/sx05re/emuelec-ports/devilutionX/package.mk
@@ -14,9 +14,10 @@ GET_HANDLER_SUPPORT="git"
 PKG_BUILD_FLAGS="-lto"
 
 pre_configure_target() {
-PKG_CMAKE_OPTS_TARGET=" -DBINARY_RELEASE=1 -DCMAKE_BUILD_TYPE="Release" -DDEBUG=OFF -DPREFILL_PLAYER_NAME=ON -DUSE_GETTEXT=ON"
+PKG_CMAKE_OPTS_TARGET=" -DBINARY_RELEASE=1 -DCMAKE_BUILD_TYPE="Release" -DDEBUG=OFF -DPREFILL_PLAYER_NAME=ON"
+#PKG_CMAKE_OPTS_TARGET=" -DBINARY_RELEASE=1 -DCMAKE_BUILD_TYPE="Release" -DDEBUG=OFF -DPREFILL_PLAYER_NAME=ON -DUSE_GETTEXT=ON"
 sed -i "s|;-static-libstdc++>|;-lstdc++>|" $PKG_BUILD/CMakeLists.txt
-sed -i "s|lang da es fr hr it pt_BR sv zh_CN|lang da es fr hr it ko_KR pt_BR sv zh_CN|" $PKG_BUILD/CMakeLists.txt
+#sed -i "s|lang da es fr hr it pt_BR sv zh_CN|lang da es fr hr it ko_KR pt_BR sv zh_CN|" $PKG_BUILD/CMakeLists.txt
 }
 
 makeinstall_target() { 
@@ -24,8 +25,8 @@ mkdir -p $INSTALL/usr/bin
 cp -rf $PKG_BUILD/.$TARGET_NAME/devilutionx $INSTALL/usr/bin
 cp -rf $PKG_BUILD/Packaging/resources/CharisSILB.ttf $PKG_BUILD/.$TARGET_NAME/devilutionx $INSTALL/usr/bin
 
-mkdir -p $INSTALL/usr/config/devilutionx
-cp -r $PKG_BUILD/.$TARGET_NAME/*.gmo $INSTALL/usr/config/devilutionx
+#mkdir -p $INSTALL/usr/config/devilutionx
+#cp -r $PKG_BUILD/.$TARGET_NAME/*.gmo $INSTALL/usr/config/devilutionx
 
 mkdir -p ${INSTALL}/usr/share/fonts/truetype
 cp ../Packaging/resources/*.ttf ${INSTALL}/usr/share/fonts/truetype/
