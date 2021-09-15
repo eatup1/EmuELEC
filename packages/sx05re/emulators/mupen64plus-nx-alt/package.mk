da9c4ffdaa651f51f046b40e5c3384a679c8fdce
Youngjun, Choi <british2@naver.com>
2021-09-15 12:52:57 +0900

    mupen64plus-nx: Add korean roms

--- packages/sx05re/emulators/mupen64plus-nx-alt/package.mk
+++ packages/sx05re/emulators/mupen64plus-nx-alt/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="mupen64plus-nx-alt"
-PKG_VERSION="7280cc27a55cc20ac16d4b6b403ca6fb22ee44c3"
+PKG_VERSION="26e306acb956e0278765babace27c7fae107ba9e"
 PKG_SHA256="eab353fe5834d256af96f1fdac328b0656bdeeebde111860067a88bf5442bfe2"
 PKG_REV="1"
 PKG_ARCH="any"
