0a8781d3663a357b81e1a95b977f2e951b78970c
Youngjun, Choi <british2@naver.com>
2021-12-03 19:10:54 +0900

    Dosbox-pure: Bump to f6b8c46

--- packages/sx05re/emulators/dosbox-pure/package.mk
+++ packages/sx05re/emulators/dosbox-pure/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="dosbox-pure"
-PKG_VERSION="01f2f5f6f3b5c2c2c58fbd4be7c271542aa84155"
+PKG_VERSION="f6b8c46650ff60c3316b0a94868ad47d925f8d29"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/schellingb/dosbox-pure"
 PKG_URL="$PKG_SITE.git"
