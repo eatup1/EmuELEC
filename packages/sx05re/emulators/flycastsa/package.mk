0e18c482716fb4596fbc23dbe45fe8418e854a36
Youngjun, Choi <british2@naver.com>
2021-12-11 13:11:05 +0900

    flycastsa: Bump to c6fe6b8

--- packages/sx05re/emulators/flycastsa/package.mk
+++ packages/sx05re/emulators/flycastsa/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="flycastsa"
-PKG_VERSION="cc208ce451ec8d5f9498b1af34b1ed14898eec03"
+PKG_VERSION="c6fe6b83ac8ceaa2c972c6df23474dbdd9ff7b32"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/flyinghead/flycast"
 PKG_URL="$PKG_SITE.git"
