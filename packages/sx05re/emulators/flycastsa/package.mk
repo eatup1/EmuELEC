5c54fda402aaa8c78dbfa6939b542e3b61b96971
Youngjun, Choi <british2@naver.com>
2022-01-26 16:33:20 +0900

    flycastsa: fix compile error

--- packages/sx05re/emulators/flycastsa/package.mk
+++ packages/sx05re/emulators/flycastsa/package.mk
@@ -6,17 +6,24 @@ PKG_VERSION="a4b0a69c455971c85257e3cfc3ee13a592296ce7"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/flyinghead/flycast"
 PKG_URL="$PKG_SITE.git"
-PKG_DEPENDS_TARGET="toolchain $OPENGLES alsa SDL2 libzip zip"
+PKG_DEPENDS_TARGET="toolchain ${OPENGLES} alsa SDL2 libzip zip"
 PKG_LONGDESC="Flycast is a multiplatform Sega Dreamcast, Naomi and Atomiswave emulator"
 PKG_TOOLCHAIN="cmake"
 PKG_GIT_CLONE_BRANCH="master"
 
-
 if [ "${ARCH}" == "arm" ]; then
 	PKG_PATCH_DIRS="arm"
 fi
-pre_configure_target() {
-PKG_CMAKE_OPTS_TARGET+="-DUSE_GLES=ON -DUSE_VULKAN=OFF"
+
+PKG_CMAKE_OPTS_TARGET="-DUSE_VULKAN=OFF \
+                        -DUSE_OPENMP=OFF \ 
+                        -DCMAKE_BUILD_TYPE=Release \
+                        -DUSE_GLES2=ON"
+
+pre_make_target() {
+  find $PKG_BUILD -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+  find $PKG_BUILD -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
+  sed -i "s|as_fn_append WARN_CXXFLAGS \" -Werror\"|as_fn_append WARN_CXXFLAGS \" -Wall\"|g" ${PKG_BUILD}/core/deps/breakpad/configure
 }
 
 makeinstall_target() {
