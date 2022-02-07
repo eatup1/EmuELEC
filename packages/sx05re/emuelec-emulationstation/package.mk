f1fb21426fd6b37f49dae1c987df054c02b61e1c
Youngjun, Choi <british2@naver.com>
2022-02-07 20:05:51 +0900

    es: Fix compile error

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="04997997dab790970baac97b3d0a73a551c4f47e"
+PKG_VERSION="2a1d10812cbbd21d6532c9a98e353857d242c298"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
