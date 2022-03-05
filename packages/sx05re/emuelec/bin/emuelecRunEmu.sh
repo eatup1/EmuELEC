9639b4d57aeb6d42967c8fc4e5ad6cb98127b089
Youngjun, Choi <british2@naver.com>
2022-03-05 12:54:55 +0900

    fbneosa: Change emulator name

--- packages/sx05re/emuelec/bin/emuelecRunEmu.sh
+++ packages/sx05re/emuelec/bin/emuelecRunEmu.sh
@@ -181,13 +181,13 @@ case ${PLATFORM} in
 		if [ "$EMU" = "AdvanceMame" ]; then
             set_kill_keys "advmame"
             RUNTHIS='${TBASH} advmame.sh "${ROMNAME}"'
-		elif [ "$EMU" = "FbneoSA" ]; then
+		elif [ "$EMU" = "FBNEOSA" ]; then
             set_kill_keys "fbneo"
             RUNTHIS='${TBASH} fbneo.sh "${ROMNAME}"'
 		fi
 		;;
 	"fbn"|"neogeo")
-        if [ "$EMU" = "FbneoSA" ]; then
+        if [ "$EMU" = "FBNEOSA" ]; then
             set_kill_keys "fbneo"
             RUNTHIS='${TBASH} fbneo.sh "${ROMNAME}"'
 		fi
@@ -261,7 +261,7 @@ case ${PLATFORM} in
 	"neocd")
 		if [ "$EMU" = "fbneo" ]; then
             RUNTHIS='${RABIN} $VERBOSE -L /tmp/cores/fbneo_libretro.so --subsystem neocd --config ${RACONF} "${ROMNAME}"'
-		elif [ "$EMU" = "FbneoSA" ]; then
+		elif [ "$EMU" = "FBNEOSA" ]; then
             set_kill_keys "fbneo"
             RUNTHIS='${TBASH} fbneo.sh "${ROMNAME}" NCD'
 		fi
