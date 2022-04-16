0750878a127a6af9f6d79b14751065654d1af3ae
Youngjun, Choi <british2@naver.com>
2022-04-16 11:52:56 +0900

    devilutionX: Bump to v1.4.0

--- packages/sx05re/emuelec-ports/devilutionX/package.mk
+++ packages/sx05re/emuelec-ports/devilutionX/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="devilutionX"
-PKG_VERSION="6099a141f347ae68818b378ccdb4260e486857c1"
+PKG_VERSION="0ac787fc054208000339f412a2bc6818661157dc"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="unlicense"
@@ -20,8 +20,6 @@ sed -i "s|;-static-libstdc++>|;-lstdc++>|" $PKG_BUILD/CMake/functions/devilution
 # Remove ja zh due to font size 
 sed -i "s|ja ko pl pt_BR ro ru uk sv zh_CN zh_TW)|ko pl pt_BR ro ru uk sv)|" $PKG_BUILD/CMake/Assets.cmake
 
-# copy korean translation
-#cp -rf $PKG_DIR/Translations/ko.po $PKG_BUILD/Translations/ko.po
 }
 
 makeinstall_target() { 
