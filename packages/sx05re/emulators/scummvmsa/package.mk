c4df9bfa90fd2ae5a32033ae33d8ef402ce5a7f8
Youngjun, Choi <british2@naver.com>
2022-05-20 18:05:35 +0900

    scummvmsa: Bump to 7d4dcf9

--- packages/sx05re/emulators/scummvmsa/package.mk
+++ packages/sx05re/emulators/scummvmsa/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="scummvmsa"
-PKG_VERSION="3cf0f78e2a597546ed5fabe9113e060974924402"
-PKG_SHA256="3101e9cbb383d512ca12f54707ac7ff403fe8f236614e0d1a64c3b6b171d8e49"
+PKG_VERSION="7d4dcf97cf7031d1ff54d2f0f1efb78a95732154"
+PKG_SHA256="5f12860e4051a4d4a4f4cb654c9a481b7fd0a8f8ccc620eb292c7a55cde02c3f"
 PKG_REV="1"
 PKG_LICENSE="GPL2"
 PKG_SITE="https://github.com/scummvm/scummvm"
