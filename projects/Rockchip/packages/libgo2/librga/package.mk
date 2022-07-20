97e5f34218ea29c5dc74691f4dc10402c10f091a
Youngjun, Choi <british2@naver.com>
2022-07-20 21:29:25 +0900

    librga: Bump to 1e2f0db

--- projects/Rockchip/packages/libgo2/librga/package.mk
+++ projects/Rockchip/packages/libgo2/librga/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2020-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="librga"
-PKG_VERSION="df26244eb0c3991df319d4276ab80e8c0bf91f64"
+PKG_VERSION="1e2f0dbb838de3512a8a29143196d7b5e460e1ca"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GNU"
 PKG_DEPENDS_TARGET="toolchain libdrm"
-PKG_SITE="https://github.com/shantigilbert/linux-rga"
+PKG_SITE="https://github.com/Caesar-github/linux-rga"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 PKG_LONGDESC="The RGA driver userspace "
 PKG_TOOLCHAIN="auto"
