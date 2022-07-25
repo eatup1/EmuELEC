aeec19298a04c019f32e3f5d55d499ba08424b7b
Youngjun, Choi <british2@naver.com>
2022-07-25 21:55:20 +0900

    lib32-retrorun: fix url

--- packages/lib32/emuelec/libretro/lib32-retrorun/package.mk
+++ packages/lib32/emuelec/libretro/lib32-retrorun/package.mk
@@ -6,7 +6,7 @@ PKG_NAME="lib32-retrorun"
 PKG_VERSION="$(get_pkg_version retrorun)"
 PKG_ARCH="aarch64"
 PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/navy1978/retrorun-go2"
+PKG_SITE="https://github.com/christianhaitian/retrorun-go2"
 PKG_URL=""
 PKG_DEPENDS_TARGET="lib32-toolchain lib32-libgo2 lib32-libdrm linux:host"
 PKG_DEPENDS_UNPACK="linux"
@@ -27,8 +27,6 @@ pre_configure_target() {
   CFLAGS+=" -I$(get_build_dir lib32-libdrm)/include/drm"
   CFLAGS+=" -I$(get_build_dir linux)/include/uapi"
   CFLAGS+=" -I$(get_build_dir linux)/tools/include"
-  sed -i "s|/storage/.config/distribution/|/emuelec/|g" ${PKG_BUILD}/src/main.cpp
-  safe_remove ${PKG_BUILD}/retrorun
 }
 
 makeinstall_target() {
