46b41c7714467aa491e1a802c2a0563f8cb79e44
Youngjun, Choi <british2@naver.com>
2022-08-07 11:38:44 +0900

    nxengine : Bump to cb4b4da

--- packages/sx05re/libretro/nxengine/package.mk
+++ packages/sx05re/libretro/nxengine/package.mk
@@ -19,8 +19,8 @@
 ################################################################################
 
 PKG_NAME="nxengine"
-PKG_VERSION="36803531a7c28000cc96f65e107370014d0c54af"
-PKG_SHA256="3470e8cada1f9dc25f1bd122ec9ab83e87b0d96ef2c5b8a21fed368856bac123"
+PKG_VERSION="cb4b4dad1182f1916abe3b16617fd37987a4a1f8"
+PKG_SHA256="2e7fa204d2790ca89f8f2bb0a21ff9eb012a2849254199c5baf04b8f291a0c0b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
