e416499b38f879547e5b872bd7ae6afde5546528
Youngjun, Choi <british2@naver.com>
2022-08-24 20:41:36 +0900

    ee: Bump to 4a358bb

--- packages/sx05re/emuelec-emulationstation/package.mk
+++ packages/sx05re/emuelec-emulationstation/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="emuelec-emulationstation"
-PKG_VERSION="f8a1cfbb53d60425e3170b37ee968d3003ae1f7c"
+PKG_VERSION="4a358bb713b9fd19ec2a436107579966b9344e96"
 PKG_GIT_CLONE_BRANCH="EmuELEC"
 PKG_REV="1"
 PKG_ARCH="any"
