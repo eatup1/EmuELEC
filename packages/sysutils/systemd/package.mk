eae0f33cdc44bb8409aa8ccc25f41a3b38b7597e
Youngjun, Choi <british2@naver.com>
2022-11-19 09:43:10 +0900

    Fix conflict

--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -3,20 +3,15 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="systemd"
-<<<<<<< HEAD
 if [ "$DEVICE" == "OdroidGoAdvance" -o "$DEVICE" == "RG351P" -o "$DEVICE" == "RG351V" ] || [ "$DEVICE" == "GameForce" ]; then
 # don't use version 250.7
 PKG_VERSION="250.6"
 PKG_SHA256="f34a9aa921cba80ec731667b8ec66c419fd78ad4469d9cba66010936bfabfd78"
 else
 # systemd 251 is required minimum kernel version 4.15
-PKG_VERSION="251.4"
-PKG_SHA256="3459239979f52b8c4ace33734d31c2fb69fa13cf81d04b1b982f7d8d4651e015"
-fi
-=======
 PKG_VERSION="252.1"
 PKG_SHA256="efd1c04d14c5e44b55d19aaf9182b309c4253bbd884374f5d840947d598e9d47"
->>>>>>> 3efb27cbb9feccb536f4dfd3fc10cc6df09141de
+fi
 PKG_LICENSE="LGPL2.1+"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/systemd"
 PKG_URL="https://github.com/systemd/systemd-stable/archive/v${PKG_VERSION}.tar.gz"
