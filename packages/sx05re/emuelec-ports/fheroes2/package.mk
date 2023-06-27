f41e41e76045c9a5e365355b7b8210efb50afe38
Youngjun, Choi <british2@naver.com>
2023-06-27 19:52:37 +0900

    fheroes2 : Bump to v1.0.5

--- packages/sx05re/emuelec-ports/fheroes2/package.mk
+++ packages/sx05re/emuelec-ports/fheroes2/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2021-present Shanti Gilbert (https://github.com/shantigilbert)
 
 PKG_NAME="fheroes2"
-PKG_VERSION="275ccfd0b4b7fc6dbf9c7498df2388fa5938541a"
+PKG_VERSION="b93f991f75d7951b0be6d5923906a78b7bc06408"
 PKG_ARCH="any"
 PKG_SITE="https://github.com/ihhub/fheroes2"
 PKG_URL="$PKG_SITE.git"
